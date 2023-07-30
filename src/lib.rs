d9b444abf223c3329d0150ba7d825cc848a2bf4a
vicky <tyagivivek528@gmail.com>
2023-07-30 06:03:05 +0530

    Testing buying function

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,49 @@
 use scrypto::prelude::*;
 
-blueprint! {
-    struct TokenSale{
-      vicky_vault:Vault,
-      xrd_vault:Vault
+#[blueprint]
+mod token_sale {
+    struct TokenSale {
+        // Define what resources and data will be managed by Hello components
+        vicky_vault: Vault,
+        xrd_vault: Vault,
+        price_per_token: Decimal,
     }
 
-    impl TokenSale{
-        pub fn new()-> ComponentAddress{
-          let bucket:Bucket = ResourceBuilder::new_fungible()
-          .metadata("name", "Vicky Token")
-          .metadata("symbol","V-X")
-          .initial_supply(100)
+    impl TokenSale {
+        // Implement the functions and methods which will manage those resources and data
+        // This is a function, and can be called directly on the blueprint once deployed
+        pub fn new(price_per_token: Decimal) -> Global<TokenSale> {
+            // Create a new token called "HelloToken," with a fixed supply of 1000, and put that supply into a bucket
+            let my_bucket: Bucket = ResourceBuilder::new_fungible(OwnerRole::None)
+                .metadata(metadata!(
+                    init {
+                        "name" => "Vicky Token", locked;
+                        "symbol" => "V-X", locked;
+                    }
+                ))
+                .mint_initial_supply(1000);
 
-        Self{
-vicky_vault:Vault  = Vault::with_bucket(bucket)
+            // Instantiate a Hello component, populating its vault with our supply of 1000 HelloToken
+            Self {
+                vicky_vault: Vault::with_bucket(my_bucket),
+                xrd_vault: Vault::new(RADIX_TOKEN), //To instantiate radix token
+                price_per_token: price_per_token,
+            }
+            .instantiate()
+            .prepare_to_globalize(OwnerRole::None)
+            .globalize()
         }
-      .instantiate()
-      .globalize()
+
+        // This is a method, because it needs a reference to self.  Methods can only be called on components
+        pub fn buy(&mut self, funds: Bucket) -> Bucket {
+            info!(
+                "My balance is: {} vicky token. Now giving away some tokens!",
+                self.vicky_vault.amount()
+            );
+
+            let purchase_amount = funds.amount() / self.price_per_token;
+            self.xrd_vault.put(funds);
+            self.vicky_vault.take(purchase_amount)
         }
     }
 }
